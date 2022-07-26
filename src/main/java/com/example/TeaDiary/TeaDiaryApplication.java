00cfa998bbcefc1f44ea163d7952e4a74f4e8e6e
NikoleMann <Nikole.d.mann@gmail.com>
2022-07-26 10:47:53 -0500

    creating repo

---
+++ src/main/java/com/example/TeaDiary/TeaDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.example.TeaDiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeaDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeaDiaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TeaDiary/TeaDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TeaDiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeaDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
